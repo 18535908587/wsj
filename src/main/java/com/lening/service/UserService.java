3967bdd646a0caafa4cf68b53dffaba2f397dca3
Think <2103191612@qq.com>
2021-04-16 15:51:23 +0800

    --[A]我在外面

---
+++ src/main/java/com/lening/service/UserService.java
@@ -0,0 +1,29 @@
+package com.lening.service;
+
+import com.lening.entity.MeunBean;
+import com.lening.entity.UserBean;
+import com.lening.utils.Page;
+
+import java.util.List;
+
+/**
+ * 创作时间：2021/4/6 10:37
+ * 作者：李增强
+ */
+public interface UserService {
+    List<UserBean> getUserList();
+
+    Page<UserBean> getUserListConn(UserBean ub, Integer pageNum, Integer pageSize);
+
+    List<MeunBean> getMeunList();
+
+    UserBean getUserVoById(Long id);
+
+    void saveUserDept(Long id, Long[] deptids);
+
+    UserBean getUserInfo(Long id);
+
+    void saveUserPost(UserBean userBean);
+
+    UserBean getLogin(UserBean ub);
+}
